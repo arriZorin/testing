d4b03252d06913a01221cfbe30a6a2a46b149453
arriZorin <ar.ri.coil@gmail.com>
2020-09-11 12:01:23 +0700

    tambah table

--- index.php
+++ index.php
@@ -13,5 +13,20 @@
 <body>
     <h1>Belajar PHP dengan menggunakan Git</h1>
     <p>dengan cara ini saya bisa belajar programming dengan lebih baik lagi</p>
+
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Arida</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Agra</td>
+        </tr>
+    </table>
 </body>
 </html>
